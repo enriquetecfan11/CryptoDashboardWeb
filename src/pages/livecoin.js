743f3e5781a6220326f03508e75b005c83f852e7
enriquetecfan11 <enriquemondejar15@gmail.com>
2022-02-19 19:19:36 +0100

    remove axios

--- src/pages/livecoin.js
+++ src/pages/livecoin.js
@@ -9,13 +9,13 @@ import {
   TableRow,
   Typography,
 } from "@mui/material";
-import axios from "axios";
 
-const getCoins = async () => {
-  const response = await axios.get(
+const getCoins = () => {
+  return fetch(
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-  );
-  return response.data;
+  )
+    .then((res) => res.json())
+    .then((data) => data);
 };
 
 const Market = () => {
